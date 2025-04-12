68eb67e899226278dab306a2d91f8c7e28afa99e
James Karnes <karnes.james@gmail.com>
2025-04-12 19:03:23 -0400

    showed prices are in DOP

--- src/components/ProductsComponents/ProductCard.tsx
+++ src/components/ProductsComponents/ProductCard.tsx
@@ -28,7 +28,10 @@ const ProductCard = ({
           <div className="truncate mr-3" translate="no">
             {product.name[selectedLang]}
           </div>
-          <div className="text-gray-500 text-sm">{product.price}</div>
+          <div className="text-gray-500 text-sm flex items-center justify-center">
+            <span className="mr-1 mt-0.5">DOP</span>
+            {product.price}
+          </div>
         </div>
         <div className={`${playfairDisplay.className} text-gray-500 text-sm`}>
           {product.shortDescription[selectedLang]}
