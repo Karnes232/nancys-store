3e81f3900174241cd405b98a55bf44be2497c4e0
James Karnes <karnes.james@gmail.com>
2025-04-20 15:08:26 -0400

    testing for dark mode in contact page

--- src/components/ContactFormComponents/ContactForm.tsx
+++ src/components/ContactFormComponents/ContactForm.tsx
@@ -49,7 +49,7 @@ const ContactForm = () => {
           type="text"
           name="name"
           id="name"
-          className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer dark:bg-black dark:text-white dark:border-gray-600"
+          className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer dark:bg-black dark:text-white dark:border-gray-600 dark:focus:text-white dark:focus:bg-black dark:[&:not(:placeholder-shown)]:bg-black"
           placeholder=" "
           required
         />
